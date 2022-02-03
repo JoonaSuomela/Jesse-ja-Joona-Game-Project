ccd37d11090b63cd9508f2a51eff2f1416361a65
Jesseasdf <41999@edu.raseko.fi>
2022-02-03 13:32:05 +0200

    Mainroom and Portalroom

--- Febeg/Assets/Scripts/MainMenu.cs
+++ Febeg/Assets/Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame () {
-        SceneManager.LoadScene("Quest1");
+        SceneManager.LoadScene("StartingRoom");
     }
 
     public void QuitGame() {
