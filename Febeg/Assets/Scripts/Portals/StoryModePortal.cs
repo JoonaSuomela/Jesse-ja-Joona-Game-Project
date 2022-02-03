75f243f4a1348e9ee3f1e64db1d51e76c1c34aff
Jesseasdf <88708031+Jesseasdf@users.noreply.github.com>
2022-02-03 20:24:52 +0200

    Added lobby and free to play room

---
+++ Febeg/Assets/Scripts/Portals/StoryModePortal.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StoryModePortal : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other){
+        SceneManager.LoadScene("Quest1");
+    }
+}
